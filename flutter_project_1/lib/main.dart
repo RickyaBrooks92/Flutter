fc93208150beb2f293fd90f3d59f2744f9492a0a
RickyTicky <Rickyabrooks92@gmail.com>
2023-01-12 20:53:23 -0500

    api call to open weather on button press

--- flutter_project_1/lib/main.dart
+++ flutter_project_1/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,10 +16,11 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Namer App',
+        title: 'flutter_project_1',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Color.fromARGB(255, 79, 254, 252)),
         ),
         home: MyHomePage(),
       ),
@@ -26,7 +29,11 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
+  String url =
+      'https://api.openweathermap.org/data/2.5/weather?lat=42.5803&lon=83.0302&units=imperial&appid=ae93a8df4eb012916dd53498f4b2cc0a';
+
+  var weatherResult = {};
+  void getWeather() async {}
 }
 
 class MyHomePage extends StatelessWidget {
@@ -35,17 +42,22 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('Random words being used'),
-          Text(appState.current.asLowerCase),
-          ElevatedButton(
-            onPressed: () {
-              print('button pressed!');
-            },
-            child: Text('Next'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Press Button To Get Weather For My City'),
+            ElevatedButton(
+              onPressed: () async {
+                Dio dio = Dio();
+                var response = await dio.get(appState.url);
+                appState.weatherResult = response.data;
+                print(appState.weatherResult);
+              },
+              child: Text('Next'),
+            ),
+          ], // ← 7
+        ),
       ),
     );
   }
